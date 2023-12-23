60cb278f6ddcf0fc305778112ba423b2b1e4185f
Florin12er <142166195+Florin12er@users.noreply.github.com>
2023-12-23 22:10:37 +0100

    add the menu button and styles when pressed for mobile phones

--- src/animation.js
+++ src/animation.js
@@ -1,5 +1,10 @@
 const hiddenElements = document.querySelectorAll(".hidden");
 const toggle = document.getElementById("switch");
+const menuImage = document.getElementById("menu");
+const nav = document.querySelector("nav");
+nav.style.height = "70px";
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
@@ -29,3 +34,21 @@ function setTheme(property, value) {
   root_theme.style.setProperty(property, value);
 }
 setTheme();
+
+menuImage.addEventListener("click", () => {
+  let closeMenu =
+    "https://github.com/Florin12er/Portofolio-website/blob/main/images/menu-close.png?raw=true";
+  let menu =
+    "https://github.com/Florin12er/Portofolio-website/blob/main/images/menu.png?raw=true";
+  if (nav.style.height === "70px") {
+    footer.style.opacity = "0.2";
+    header.style.opacity = "0.2";
+    menuImage.src = closeMenu;
+    nav.style.height = "250px";
+  } else {
+    header.style.opacity = "1";
+    footer.style.opacity = "1";
+    menuImage.src = menu;
+    nav.style.height = "70px";
+  }
+});
